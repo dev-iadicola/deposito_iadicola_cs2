4a4c8a2bf213a5f8c024d8a5b74f088e2708cb68
Luigi Iadicola <luigiadicola@gmail.com>
2025-10-20 16:30:09 +0200

    DI

--- DependecyInjection/Program.cs
+++ DependecyInjection/Program.cs
@@ -38,25 +38,44 @@ public interface IGreeter
 
 public class ConsoleGreeter : IGreeter
 {
-
+    public void benvenuto()
+    {
+       System.Console.WriteLine("Welcome to City 17. \nYou have chosen, or been chosen, to relocate to one of our finest remaining urban centers. ");
+    }
 }
 #endregion
 
 #region Greeting service 
 public class GreetingService
 {
-    public void benvenuto();
+    private readonly IGreeter _cg;
+    public GreetingService(IGreeter iGreete)
+    {
+        _cg = iGreete;
+    }
+    
+    public void Greeting()
+    {
+        _cg.benvenuto();   
+    }
 }
 
+#endregion
+
+#region Interfaccia Payment 
+public interface IPaymentGetway
+{
+    public 
+}
 #endregion
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        ILogger logger = new ConsoleLogger();
-        UserService userService = new UserService(logger);
-        userService.CreateUser("Alice");
+        // IGreeter gr = new ConsoleGreeter();
+        // GreetingService grs = new GreetingService(gr);
+        // grs.Greeting();
 
     }
 
