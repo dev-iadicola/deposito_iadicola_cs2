98b4c45c2e2ce02907fc5ce08e04cef7daf3e586
Luigi Iadicola <luigiadicola@gmail.com>
2025-10-23 10:24:23 +0200

    Delegati esempio

---
+++ Delegati/Program.cs
@@ -0,0 +1,19 @@
+﻿// Definizione del delegate
+delegate void Saluto(string nome);
+
+class Program
+{
+    static void Main()
+    {
+        // Assegno il metodo Ciao al delegate
+        Saluto s = Ciao;
+
+        // Invoco il delegate
+        s("Mario");
+    }
+
+    static void Ciao(string nome)
+    {
+        Console.WriteLine($"Ciao, {nome}");
+    }
+}
---
+++ Delegati/Vendor/Test/ITest.cs
@@ -0,0 +1,9 @@
+
+namespace N_tier.Vendor.Test;
+public interface ITest
+{
+    string Name { get; }
+    void Run();
+}
+
+
