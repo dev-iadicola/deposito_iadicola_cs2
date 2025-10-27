3b4f7050330a60df2cc68d531b8be5e8bafba179
Luigi Iadicola <luigiadicola@gmail.com>
2025-10-27 12:00:38 +0100

    Esercizio Sql: ordini e clienti (join)

---
+++ SQL/27-10-2025/es.5.ordini.sql
@@ -0,0 +1,64 @@
+
+use world;
+CREATE TABLE IF NOT EXISTS ordini (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  id_cliente INT NULL,
+  data_ordine DATE NOT NULL,
+  importo DECIMAL(7,2) NOT NULL,
+  FOREIGN KEY (id_cliente) REFERENCES clienti(id) ON DELETE SET NULL
+  -- in caso il cliente non sopravvive alla catastrofe, conserviamo l'ordine
+);
+
+-- popolamento 
+INSERT INTO ordini (id_cliente, data_ordine, importo)
+SELECT
+  FLOOR(1 + RAND() * 35) AS id_cliente,
+  DATE_ADD('2024-01-01', INTERVAL FLOOR(RAND() * 222) DAY) AS data_ordine,
+  ROUND(10 + (RAND() * 990), 2) AS importo
+FROM
+  information_schema.columns AS t1,
+  information_schema.columns AS t2
+LIMIT 200;
+
+-- popolamneot ordini null
+INSERT INTO ordini (id_cliente, data_ordine, importo)
+SELECT
+  NULL AS id_cliente,
+  DATE_ADD('2024-01-01', INTERVAL FLOOR(RAND() * 222) DAY) AS data_ordine,
+  ROUND(10 + (RAND() * 990), 2) AS importo
+FROM
+  information_schema.columns AS t1,
+  information_schema.columns AS t2
+LIMIT 200;
+SELECT 
+o.data_ordine, c.nome
+FROM ordini o
+left JOIN clienti c ON o.id_cliente = c.id
+;
+
+SELECT 
+o.data_ordine, c.nome
+FROM ordini o
+left JOIN clienti c ON o.id_cliente = c.id
+;
+
+-- 100 senza clienti
+SELECT COUNT(*) FROM ordini 
+WHERE id_cliente IS NULL;
+
+-- tutti ordini: anche senza ordini 
+SELECT 
+o.data_ordine, c.nome
+FROM clienti c
+right JOIN ordini o ON o.id_cliente = c.id
+;
+
+-- caso 3
+SELECT 
+o.data_ordine, c.nome
+FROM ordini o
+left JOIN clienti c ON o.id_cliente = c.id
+ORDER BY o.id_cliente;
+
+
+
